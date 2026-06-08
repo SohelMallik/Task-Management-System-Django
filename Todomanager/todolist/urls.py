8b451eaf1f6bd8539c26057a01fe547081f6269b
Sohel Mallik <malliksohel582@gmail.com>
2026-06-08 09:11:11 +0530

    Commend

--- Todomanager/todolist/urls.py
+++ Todomanager/todolist/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from . import views
-
+#Connect Urls with Views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('contact/', views.contact, name='contact'),
--- Todomanager/todolist/views.py
+++ Todomanager/todolist/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Views connect with Templates
 def homepage(request):
     return render(request, 'main.html', {})
 
