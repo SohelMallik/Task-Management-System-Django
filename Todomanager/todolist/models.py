359f2163ab2bcbdf1ec00e3c40db9dcaddaf5adb
Sohel Mallik <malliksohel582@gmail.com>
2026-06-09 22:20:28 +0530

    Models

--- Todomanager/todolist/models.py
+++ Todomanager/todolist/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):#Tell it is model
+    task= models.CharField(max_length=200) #Task is a character field with max length of 200
+    is_completed= models.BooleanField(default=False) #is_completed is a boolean field with default value of False
+
