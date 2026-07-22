25fe6e5d2388570500c357a11f95ec0702fac450
Sohel Mallik <malliksohel582@gmail.com>
2026-07-22 18:41:52 +0530

    Todomanager/settings.py

--- Todomanager/Todomanager/settings.py
+++ Todomanager/Todomanager/settings.py
@@ -120,7 +120,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
+]
+
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
 ]
